7b160878fe307df6095bad4a24c1ba923bc3e8b4
Joel Richard <cajunjoel@github.org>
2022-03-17 11:48:20 -0400

    Added abstracts, improved css

--- js/silibraries-sro.js
+++ js/silibraries-sro.js
@@ -1,9 +1,29 @@
 function sroToggleAdvancedSearch() {
-	el = document.getElementById('advanced-search');
-	if (el.style.display == 'none') {
-		el.style.display = 'block';
+	elAdvanced = document.getElementById('advanced-search');
+	if (elAdvanced.style.display == 'none') {
+		elAdvanced.style.display = 'block';
 	} else {
-		el.style.display = 'none';	
+		elAdvanced.style.display = 'none';	
 	}
 }
 
+function sroToggleAbstract(elButton) {
+  elAbstract = document.getElementById('abstract-'+elButton.dataset.id);
+	if (elButton.innerText == 'More...') {
+		elAbstract.classList.add('visible');
+		elButton.innerText = 'Less...';
+	} else {
+		elAbstract.classList.remove('visible');
+		elButton.innerText = 'More...';
+	}  
+}
+
+function reloadPage() {
+  spin = document.getElementsByClassName('spinner');
+  for (i=0; i<spin.length; i++) {
+    spin[i].style.display = 'inline-block';
+  }
+  frm = document.getElementById('sro_results_search');
+  frm.submit();
+  return false;
+}
